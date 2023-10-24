6e1bb729ae8c3050ce1f21f9dd613e628d3044ea
João Lucas <jloc@ic.ufal.br>
2023-10-23 21:04:46 -0300

    fix: more things

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
 python manage.py loaddata utils/books_fixture.json
+python manage.py loaddata utils/genres_fixture.json
 
 exec "$@"
