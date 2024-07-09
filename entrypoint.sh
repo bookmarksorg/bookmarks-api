71cee7679d4043a7d09fc566f7afde380bf75fad
João Lucas <jloc@ic.ufal.br>
2024-07-09 19:41:58 -0300

    fix: trying using gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 # python manage.py flush --no-input
 python manage.py migrate
-python manage.py collectstatic --no-input --clear
-python manage.py createsuperuser --no-input
 python manage.py loaddata utils/genres_fixture.json
 python manage.py loaddata utils/books_fixture.json
+# python manage.py collectstatic --no-input --clear
+gunicorn --bind :8000 --workers 3 setup.wsgi:application
 
 exec "$@"
